a7e139b598979ea99e7fefdf9a3559bc10d8763b
pyroNun <33143095+pyroNun@users.noreply.github.com>
2022-06-16 20:24:41 +0200

    init

---
+++ Tutorial1/mvc_backend/mvc_backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_backend.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public String Index()
+        {
+            return "This is the index page";
+        }
+
+        public String Hello()
+        {
+            return "Whos's there";
+        }
+    }
+}
